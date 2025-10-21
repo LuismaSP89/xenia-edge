2abed3f5ae3f98dc7aa89a2118d5175a9043428a
Herman S. <429230+has207@users.noreply.github.com>
2025-10-21 10:54:12 +0900

    [App] ensure InitFeatureFlags is called after cvar parsing
    Fixes x64_exension_mask being applied

--- src/xenia/app/xenia_main.cc
+++ src/xenia/app/xenia_main.cc
@@ -462,9 +462,6 @@ std::vector<std::unique_ptr<hid::InputDriver>> EmulatorApp::CreateInputDrivers(
 }
 
 bool EmulatorApp::OnInitialize() {
-#if XE_ARCH_AMD64 == 1
-  amd64::InitFeatureFlags();
-#endif
   Profiler::Initialize();
   Profiler::ThreadEnter("Main");
 
@@ -489,6 +486,10 @@ bool EmulatorApp::OnInitialize() {
   XELOGI("Storage root: {}", storage_root);
 
   config::SetupConfig(storage_root);
+
+#if XE_ARCH_AMD64 == 1
+  amd64::InitFeatureFlags();
+#endif
 
   std::filesystem::path content_root = cvars::content_root;
   if (content_root.empty()) {
