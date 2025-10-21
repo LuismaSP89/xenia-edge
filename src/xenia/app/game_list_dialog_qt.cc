dcb925bca0973c2aa4a6b317523e4d67de7aa16e
Herman S. <429230+has207@users.noreply.github.com>
2025-10-21 12:26:34 +0900

    [UI] Add clear button to the search bar

--- src/xenia/app/game_list_dialog_qt.cc
+++ src/xenia/app/game_list_dialog_qt.cc
@@ -225,6 +225,7 @@ void GameListDialogQt::SetupUI() {
   // Search box
   search_box_ = new QLineEdit(this);
   search_box_->setPlaceholderText("Search games...");
+  search_box_->setClearButtonEnabled(true);
   QFont search_font = search_box_->font();
   search_font.setPointSize(16);
   search_box_->setFont(search_font);
