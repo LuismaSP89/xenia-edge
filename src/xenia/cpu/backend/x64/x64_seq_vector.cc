d8abd26447eac04a260b56d9967a50a0ecf7f30a
Herman S. <429230+has207@users.noreply.github.com>
2025-10-23 15:50:09 +0900

    x64 seq vector disable final 1

--- src/xenia/cpu/backend/x64/x64_seq_vector.cc
+++ src/xenia/cpu/backend/x64/x64_seq_vector.cc
@@ -816,8 +816,8 @@ struct VECTOR_SUB
                 } else {
                   e.vpsubd(e.xmm1, src1, src2);
 
-                  if (e.IsFeatureEnabled(kX64EmitAVX512Ortho |
-                                         kX64EmitAVX512DQ)) {
+                  if (false && e.IsFeatureEnabled(kX64EmitAVX512Ortho |
+                                                  kX64EmitAVX512DQ)) {
                     e.vmovdqa32(e.xmm3, src1);
                     e.vpternlogd(e.xmm3, e.xmm1, src2, 0b00011000);
 
